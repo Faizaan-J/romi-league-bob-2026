a1d6a6ccb405c14a518898a63a1acea0f6d1ddcf
Faizaan J <125227430+Faizaan-J@users.noreply.github.com>
2025-12-12 17:15:49 -0500

    Draft BlueCurve auton

--- src/main/java/frc/robot/autons/BlueAutons.java
+++ src/main/java/frc/robot/autons/BlueAutons.java
@@ -2,30 +2,39 @@ package frc.robot.autons;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.DriveDistance;
-import frc.robot.commands.TurnDegrees;
+import frc.robot.commands.DriveTurn;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.enums.QUADRANT;
 
 public class BlueAutons extends SequentialCommandGroup {
     private Drivetrain drivetrain;
 
-    private void registerRedCurve() {
-
+    private void registerBlueCurve() {
+        addCommands(
+            new DriveDistance(1, 9, drivetrain),
+            new DriveTurn(1, 90, 0, drivetrain),
+            new DriveDistance(1, 13.73, drivetrain),
+            new DriveTurn(1, 90, 0, drivetrain),
+            new DriveDistance(1, 10.5, drivetrain),
+            new DriveTurn(1, -90, 0, drivetrain),
+            new DriveDistance(1, 12.6, drivetrain),
+            new DriveTurn(1, -90, 0, drivetrain),
+            new DriveDistance(1, 8, drivetrain),
+            new DriveTurn(1, 90, 0, drivetrain),
+            new DriveTurn(1, 26.3, 9.62, drivetrain)
+        );
     }
 
-    private void registerRedTriangle() {
-        addCommands(
-            new DriveDistance(1, 6, drivetrain),
-            new TurnDegrees(1, 90, drivetrain)
-        );
+    private void registerBlueTriangle() {
+
     }
 
     public BlueAutons(Drivetrain drivetrain, QUADRANT quadrant) {
         this.drivetrain = drivetrain;
         if (quadrant == QUADRANT.CURVE) {
-            registerRedCurve();
+            registerBlueCurve();
         } else if (quadrant == QUADRANT.TRIANGLE) {
-            registerRedTriangle();
+            registerBlueTriangle();
         }
     }
 }
